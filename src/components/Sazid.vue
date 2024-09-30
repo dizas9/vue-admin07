8c2475bf2f244173fede3ea43a80abe80c407cc7
dizas9 <sazid.ice07@gmail.com>
2024-09-30 20:18:31 +0600

    first basic

---
+++ src/components/Sazid.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>Hello ,{{ name }}</h1>
+</template>
